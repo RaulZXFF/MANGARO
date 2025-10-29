244f73158639be8862563a4fed9d62c89bb32882
TXRROT <65623764+txrrot@users.noreply.github.com>
2025-10-29 14:13:47 +0200

    Update reader.js

--- reader.js
+++ reader.js
@@ -67,7 +67,7 @@ document.addEventListener('keydown', e => {
 imgEl.addEventListener('click', e => { 
   const rect = imgEl.getBoundingClientRect(); 
   const x = e.clientX - rect.left; 
-  x < rect.width / 2 ? clickNext() : clickPrev(); 
+  x < rect.width / 2 ? clickPrev() : clickNext(); 
 })
 
 if(slider){
